c122638740df837d1c176b2e4336327965e7bc74
swingzhou <337043944@qq.com>
2019-01-23 10:19:50 +0800

    no message

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "xlink-ship-rest",
+  "version": "0.0.1",
+  "description": "出货业务服务接口",
+  "title": "xlink-ship-rest",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-01-22T10:02:17.558Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
